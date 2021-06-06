29b6993f1e46472cf9e59c541363231c0df2e6a4
Aleksey Burtsev <onery1176@gmail.com>
2021-06-06 19:11:38 +0300

    update

--- js/task-08.js
+++ js/task-08.js
@@ -9,12 +9,12 @@ const refs = {
 
 };
 
-const galleryMarkup = gallery.map(({ preview, original, description }, el) =>
+const galleryMarkup = gallery.map(({ preview, original, description }, index) =>
     `
 <li class = "gallery__item">
 <a class = "gallery__link" href = " ">
 <img class = "gallery__image" src = "${preview}" data-source = "${original}"
-alt = "${description}" data-index = "${el}">
+alt = "${description}" data-index = "${index}">
 </a>
 </li>
 `).join('');
@@ -22,6 +22,44 @@ alt = "${description}" data-index = "${el}">
 console.log(galleryMarkup);
 
 refs.jsGalleryRef.innerHTML = galleryMarkup;
+
+const openModalClick = element => {
+    
+    element.preventDefault();
+
+    if (element.target.localName === 'img') {
+        
+        refs.modalImageRef.src = element.target.dataset.source;
+        refs.modalImageRef.alt = element.target.alt;
+        refs.modalImageRef.dataset.index = element.target.dataset.index;
+        refs.modalRef.classList.add('is-open');
+    }
+};
+
+const escClick = element => {
+    
+    if (element.key === 'Escape') {
+        
+        refs.modalRef.classList.remove('is-open')
+    }
+};
+
+const closeModalClick = element  => {
+    if (element.target.localName !== 'img') {
+        refs.modalRef.classList.remove('is-open');
+
+        refs.modalImageRef.src = '';
+        refs.modalImageRef.alt = '';
+
+    }
+
+};
+
+refs.jsGalleryRef.addEventListener('click', openModalClick);
+window.addEventListener('click', closeModalClick);
+window.addEventListener('keydown', escClick);
+
+
 
 
 
